c1e7bc25678d8d4ed3b325f023f2dd2f26b6fcb3
Gopal Mahato <gopalmahatomain2026@gmail.com>
2025-07-21 12:45:57 +0530

    Updating compound_interest.py file

--- 01-python-programming/basics/problems/compound_interest.py
+++ 01-python-programming/basics/problems/compound_interest.py
@@ -1,4 +1,12 @@
 # compound_interest.py: Compute compound interest (P, R, T).  CI=P(1+i)^n −P
+compound_interest.pyp = float(input("Enter principal: "))
+r = float(input("Enter rate of interest (in decimal): "))
+t = int(input("Enter time (years): "))
+n = int(input("Compounded how many times per year? "))
+
+amount = p * (1 + r / n) ** (n * t)
+print("Compound Interest:", round(amount - p, 2))
+print("Final Amount:", round(amount, 2))
 principal = 1000
 rate = 0.05
 time = 10
